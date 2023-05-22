414edfcadc0c9cdba0126a938e13c78965d09a1e
awqsomee <awqsome@bk.ru>
2023-05-22 23:01:30 +0300

    Реализовал новую архитекуру API

--- app/src/main/java/com/example/stonksexchange/api/AuthApiService.java
+++ app/src/main/java/com/example/stonksexchange/api/AuthApiService.java
@@ -3,16 +3,20 @@ package com.example.stonksexchange.api;
 import com.example.stonksexchange.api.domain.auth.AuthResponse;
 import com.example.stonksexchange.api.domain.auth.LoginRequest;
 import com.example.stonksexchange.api.domain.auth.SignUpRequest;
-import com.example.stonksexchange.api.domain.balance.ChangeBalanceRequest;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
-import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 
-public interface ApiService extends AuthApiService, BalanceApiService{
+public interface AuthApiService {
+    @POST("auth/login")
+    Call<AuthResponse> logIn(@Body LoginRequest request);
 
-    ApiService ApiService = ApiManager.getApiService();
+    @POST("auth/registration")
+    Call<AuthResponse> signUp(@Body SignUpRequest request);
+
+    @GET("auth")
+    Call<AuthResponse> auth(@Header("Authorization") String AuthHeader);
 }
