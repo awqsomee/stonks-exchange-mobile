6d4bb6509aa0d68a5a9b793d6c07523b3d4552af
awqsomee <awqsome@bk.ru>
2023-05-29 13:13:52 +0300

    Добавил запрос на получении информации об акции

--- app/src/main/java/com/example/stonksexchange/fragment/StockFragment.java
+++ app/src/main/java/com/example/stonksexchange/fragment/StockFragment.java
@@ -6,16 +6,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.stonksexchange.App;
 import com.example.stonksexchange.R;
+import com.example.stonksexchange.api.ApiService;
+import com.example.stonksexchange.api.ErrorUtils;
+import com.example.stonksexchange.api.domain.balance.ChangeBalanceResponse;
+import com.example.stonksexchange.api.domain.stock.GetStockDataResponse;
 import com.example.stonksexchange.utils.BackButtonHandler;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class StockFragment extends Fragment {
     App app;
     Context context;
+    String symbol;
+    TextView textView;
 
     public StockFragment() {
     }
@@ -35,11 +46,34 @@ public class StockFragment extends Fragment {
         app = App.getInstance();
         context = view.getContext();
         BackButtonHandler.setupBackPressedCallback(this);
-        String symbol = getArguments().getString("symbol");
+        symbol = getArguments().getString("symbol");
 
-        TextView textView = view.findViewById(R.id.textView2);
+         textView = view.findViewById(R.id.textView2);
         textView.setText(symbol);
 
+        getStockData();
+
         return view;
+    }
+
+    private void getStockData() {
+        Call<GetStockDataResponse> call = ApiService.ApiService.getStockData(symbol, "", "");
+        call.enqueue(new Callback<GetStockDataResponse>() {
+            @Override
+            public void onResponse(Call<GetStockDataResponse> call, Response<GetStockDataResponse> response) {
+                if (response.isSuccessful()) {
+                    GetStockDataResponse data = response.body();
+                    textView.setText(data.getStock().getName());
+                } else {
+                    ErrorUtils.handleErrorResponse(response, context);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GetStockDataResponse> call, Throwable t) {
+                Toast.makeText(context, t.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
