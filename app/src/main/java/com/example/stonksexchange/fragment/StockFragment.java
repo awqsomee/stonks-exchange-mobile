ba4148e4f8bd2519c457b48bb158be1bcf1b1a4c
nerpyshka <71436617+ValeriaNigametzianova@users.noreply.github.com>
2023-06-01 02:04:26 +0300

    Создала график
    Тут библиотечка с гита подгруженная, на компоновке добавлен элемент и прописаны некоторые настройки в коде

--- app/src/main/java/com/example/stonksexchange/fragment/StockFragment.java
+++ app/src/main/java/com/example/stonksexchange/fragment/StockFragment.java
@@ -1,6 +1,8 @@
 package com.example.stonksexchange.fragment;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.stonksexchange.App;
@@ -16,6 +19,18 @@ import com.example.stonksexchange.api.ApiService;
 import com.example.stonksexchange.api.ErrorUtils;
 import com.example.stonksexchange.api.domain.stock.GetStockDataResponse;
 import com.example.stonksexchange.utils.BackButtonHandler;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.IMarker;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -26,6 +41,8 @@ public class StockFragment extends Fragment {
     Context context;
     String symbol;
     TextView textView;
+
+    private LineChart chart;
 
     public StockFragment() {
     }
@@ -47,11 +64,12 @@ public class StockFragment extends Fragment {
         BackButtonHandler.setupBackPressedCallback(this);
         symbol = getArguments().getString("symbol");
 
-         textView = view.findViewById(R.id.stockFullname);
+        textView = view.findViewById(R.id.stockFullname);
         textView.setText(symbol);
 
         getStockData();
-
+        chart = view.findViewById(R.id.chart);
+        setChartData();
         return view;
     }
 
@@ -75,4 +93,62 @@ public class StockFragment extends Fragment {
             }
         });
     }
+
+    private void setChartData(){
+        // Массив координат точек
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(1f, 5f));
+        entries.add(new Entry(2f, 2f));
+        entries.add(new Entry(3f, 1f));
+        entries.add(new Entry(4f, 3f));
+        entries.add(new Entry(5f, 4f));
+        entries.add(new Entry(6f, 1f));
+
+        LineDataSet dataset = new LineDataSet(entries, "График первый");
+        dataset.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        dataset.setColors(new int[] {R.color.green}, context);    //цвет линии графика
+
+        Drawable drawable = ContextCompat.getDrawable(context, R.drawable.chart_gradients);
+        dataset.setDrawFilled(true);
+        dataset.setFillDrawable(drawable);
+
+//        dataset.setGradientColor(getResources().getColor(R.color.green), getResources().getColor(R.color.app_background));
+        dataset.setValueTextSize(10);
+        dataset.setValueTextColor(getResources().getColor(R.color.white));
+
+        dataset.setHighlightEnabled(true);  //линии при выделенной точке
+        dataset.setHighLightColor(getResources().getColor(R.color.light_grey));
+
+        LineData data = new LineData(dataset);
+
+        IMarker marker = chart.getMarker();
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setAxisLineColor(Color.parseColor("#44454B"));
+        xAxis.setTextColor(getResources().getColor(R.color.white));
+        xAxis.setTextSize(10);
+
+        YAxis yAxis = chart.getAxisLeft();
+        yAxis.setDrawLabels(false);
+        yAxis.setDrawGridLines(false);
+        yAxis.setDrawZeroLine(true);
+        yAxis.setDrawAxisLine(false);
+
+        Legend legend = chart.getLegend();
+        legend.setEnabled(false);
+//        legend.setTextColor(Color.parseColor("#EE8100"));
+        Description description = chart.getDescription();
+        description.setEnabled(false);
+
+        chart.getAxisRight().setEnabled(false);
+        chart.setGridBackgroundColor(getResources().getColor(R.color.orange));
+        chart.setData(data);
+        chart.setAutoScaleMinMaxEnabled(true);
+        chart.setKeepPositionOnRotation(true);
+        chart.setPadding(0,0,0,0);
+        chart.invalidate();
+    }
+
 }
